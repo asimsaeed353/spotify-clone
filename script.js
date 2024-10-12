6b184d0c9438f1f4383bf07d5591c31797a47c5c
M Asim Saeed <asimsaeed353@gmail.com>
2024-10-13 00:08:09 +0500

    Update the index.html and opacity of play and previous

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 console.log("Hello, this is javaScript");
 let currentSong = new Audio();
 const songs = [
-  '/audios/aj-bazar-mein-pabajulaan.mp3',
-  '/audios/aj-dil-k-qareen.mp3',
-  '/audios/ap-ki-yad-aati-rahi.mp3',
-  '/audios/aye-khuch-abrr.mp3',
-  '/audios/dard-itna-tha.mp3',
-  '/audios/dasht-e-tanhai-mein.mp3',
-  '/audios/gar-mujhey-iska-yaqeen.mp3',
-  '/audios/garmi-e-shoq-e-nazara.mp3',
-  '/audios/gulon-mein-rang-bhary.mp3',
-  '/audios/hijr-ki-raakh.mp3',
-  '/audios/hum-jo-tareek-raahon-mein.mp3',
-  '/audios/hum-ne-wo-sab.mp3',
-  '/audios/hum-par-tumhari-chah-ka.mp3',
-  '/audios/tum-aye-ho-na-shab-e-intazar.mp3',
+  "/audios/aj-bazar-mein-pabajulaan.mp3",
+  "/audios/aj-dil-k-qareen.mp3",
+  "/audios/ap-ki-yad-aati-rahi.mp3",
+  "/audios/aye-khuch-abrr.mp3",
+  "/audios/dard-itna-tha.mp3",
+  "/audios/dasht-e-tanhai-mein.mp3",
+  "/audios/gar-mujhey-iska-yaqeen.mp3",
+  "/audios/garmi-e-shoq-e-nazara.mp3",
+  "/audios/gulon-mein-rang-bhary.mp3",
+  "/audios/hijr-ki-raakh.mp3",
+  "/audios/hum-jo-tareek-raahon-mein.mp3",
+  "/audios/hum-ne-wo-sab.mp3",
+  "/audios/hum-par-tumhari-chah-ka.mp3",
+  "/audios/tum-aye-ho-na-shab-e-intazar.mp3",
 ];
 
 let selectedSong = new Audio();
@@ -38,11 +38,13 @@ let selectedSong = new Audio();
 
 function playMusic(track) {
   // currentSong.src = './audios/' + track + '.mp3';
-  selectedSong.src = './audios/' + track + '.mp3';
+  selectedSong.src = "./audios/" + track + ".mp3";
   // currentSong.src = track;
   // currentSong.play();
   selectedSong.play();
-  document.querySelector(".play-pause").innerHTML = `<i id="pause" class="ri-pause-fill pointer"></i>`;
+  document.querySelector(
+    ".play-pause"
+  ).innerHTML = `<i id="pause" class="ri-pause-fill pointer"></i>`;
   document.querySelector(".song-player__song-name").innerHTML = decodeURI(
     track.replaceAll("-", " ")
   );
@@ -56,7 +58,7 @@ async function main() {
 
   function formatTime(seconds) {
     if (isNaN(seconds) || seconds < 0) {
-      return '00:00';
+      return "00:00";
     }
 
     const minutes = Math.floor(seconds / 60);
@@ -121,7 +123,9 @@ async function main() {
       ).innerHTML = `<i id="pause" class="ri-pause-fill pointer"></i>`;
     } else {
       selectedSong.pause();
-      document.querySelector(".play-pause").innerHTML = `<i id="play" class="ri-play-fill pointer"></i>`;
+      document.querySelector(
+        ".play-pause"
+      ).innerHTML = `<i id="play" class="ri-play-fill pointer"></i>`;
     }
   });
 
@@ -131,81 +135,106 @@ async function main() {
       formatTime(selectedSong.currentTime),
       formatTime(selectedSong.duration)
     );
-    
+
     // Update Song timing / duration
     document.querySelector(".song-duration").innerHTML = `${formatTime(
       selectedSong.currentTime
     )}/${formatTime(selectedSong.duration)}`;
 
     // Move seekbar circle
-    document.querySelector('.circle').style.left = (selectedSong.currentTime / selectedSong.duration) * 100 + "%";
+    document.querySelector(".circle").style.left =
+      (selectedSong.currentTime / selectedSong.duration) * 100 + "%";
     // Come back to original position when song is finished
-    if (((selectedSong.currentTime / selectedSong.duration) * 100) > 99) {
-      document.querySelector('.circle').style.left = 0 + '%';
-      document.querySelector(".play-pause").innerHTML = `<i id="play" class="ri-play-fill pointer"></i>`;
+    if ((selectedSong.currentTime / selectedSong.duration) * 100 > 99) {
+      document.querySelector(".circle").style.left = 0 + "%";
+      document.querySelector(
+        ".play-pause"
+      ).innerHTML = `<i id="play" class="ri-play-fill pointer"></i>`;
     }
   });
 
   // add an Event listener to seek bar
-  document.querySelector('.seek-bar').addEventListener('click', e => {
+  document.querySelector(".seek-bar").addEventListener("click", (e) => {
     console.log(e.target.getBoundingClientRect().width, e.offsetX);
     let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
-    document.querySelector('.circle').style.left = percent + '%';
+    document.querySelector(".circle").style.left = percent + "%";
     selectedSong.currentTime = (selectedSong.duration * percent) / 100;
-  })
+  });
 
   // Responsive left show on hamburger touch
-  document.querySelector('.hamburger-icon').addEventListener('click', () => {
-    document.querySelector('.left').style.left = '0%';
-    document.querySelector('.left').style.width = '50%';
-  })
+  document.querySelector(".hamburger-icon").addEventListener("click", () => {
+    document.querySelector(".left").style.left = "0%";
+    document.querySelector(".left").style.width = "50%";
+  });
 
   // Add an event listener to the close icon to close the left part in smaller devices
-  document.querySelector('.close-icon').addEventListener('click', () => {
-    document.querySelector('.left').style.left =  '-110%';
-  })
+  document.querySelector(".close-icon").addEventListener("click", () => {
+    document.querySelector(".left").style.left = "-110%";
+  });
 
   // Add and event listener to the previous icon
-  previous.addEventListener('click', () => {
+  previous.addEventListener("click", () => {
+    console.log("Previous is clicked.");
+    let index = songs.indexOf(
+      selectedSong.src.replace(
+        "https://asimsaeed353.github.io/spotify-clone",
+        ""
+      )
+    );
 
-    console.log('Previous is clicked.');
-    let index = songs.indexOf(selectedSong.src.replace('https://asimsaeed353.github.io/spotify-clone', ''));
-    
-    console.log('Previous is clicked so here is the index of song', selectedSong.src, selectedSong.src.replace('https://asimsaeed353.github.io/spotify-clone', ''),index);
+    console.log(
+      "Previous is clicked so here is the index of song",
+      selectedSong.src,
+      selectedSong.src.replace(
+        "https://asimsaeed353.github.io/spotify-clone",
+        ""
+      ),
+      index
+    );
 
-    if ((index - 1) >= 0) {
-      playMusic(songs[index - 1].replace('/audios/', '').replace('.mp3', ''));
-      previous.style.opacity = '1';
-      next.style.opacity = '1';
+    if (index - 1 >= 0) {
+      playMusic(songs[index - 1].replace("/audios/", "").replace(".mp3", ""));
+      previous.style.opacity = "1";
+      next.style.opacity = "1";
+    } else if (index - 1 < 0) {
+      previous.style.opacity = "0.5";
     }
-    else if ((index - 1) < 0) {
-      previous.style.opacity = '0.7';
-    }
-  })
+  });
 
   // Add and event listener to the next icon
-  next.addEventListener('click', () => {
-
-    console.log('next is clicked.');
+  next.addEventListener("click", () => {
+    console.log("next is clicked.");
 
     // let index = songs.indexOf(selectedSong.src);
-    let index = songs.indexOf(selectedSong.src.replace('https://asimsaeed353.github.io/spotify-clone', ''));
-    console.log('Next is clicked so here is the index of song', selectedSong.src, selectedSong.src.replace('https://asimsaeed353.github.io/spotify-clone', ''),index);
-    if ((index + 1) < songs.length) {
-      playMusic(songs[index + 1].replace('/audios/', '').replace('.mp3', ''));
-      previous.style.opacity = '1';
-      next.style.opacity = '1';
+    let index = songs.indexOf(
+      selectedSong.src.replace(
+        "https://asimsaeed353.github.io/spotify-clone",
+        ""
+      )
+    );
+    console.log(
+      "Next is clicked so here is the index of song",
+      selectedSong.src,
+      selectedSong.src.replace(
+        "https://asimsaeed353.github.io/spotify-clone",
+        ""
+      ),
+      index
+    );
+    if (index + 1 < songs.length) {
+      playMusic(songs[index + 1].replace("/audios/", "").replace(".mp3", ""));
+      previous.style.opacity = "1";
+      next.style.opacity = "1";
+    } else if (index + 1 >= songs.length) {
+      next.style.opacity = "0.5";
     }
-    else if ((index + 1) >= songs.length) {
-      next.style.opacity = '0.7';
-    }
-  })
+  });
 
   // Add event listener to the range
-  document.querySelector('.slider').addEventListener('change', (e) => {
-    console.log('setting volume to ', e.target.value);
+  document.querySelector(".slider").addEventListener("change", (e) => {
+    console.log("setting volume to ", e.target.value);
     selectedSong.volume = parseInt(e.target.value) / 100;
-  })
+  });
 }
 
 main();
